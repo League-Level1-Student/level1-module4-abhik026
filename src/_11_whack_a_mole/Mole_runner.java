b6c012f30d2e87d2579e6895b028a8d950bedb9d
kumar <kumar@LAPTOP-182M7PCD.lan>
2021-04-02 17:03:24 -0700

    on mole

---
+++ src/_11_whack_a_mole/Mole_runner.java
@@ -0,0 +1,10 @@
+package _11_whack_a_mole;
+
+public class Mole_runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new mole().run();
+	}
+
+}
--- src/_11_whack_a_mole/mole.java
+++ src/_11_whack_a_mole/mole.java
@@ -1,5 +1,82 @@
 package _11_whack_a_mole;
 
-public class mole {
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class mole implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton("MOLE!");
+	Random ran = new Random();
+	int mole = ran.nextInt(24);
+	int count = 0;
+	public void run() {
+		drawButtons(mole);
+	}
+
+	void drawButtons(int location) {
+		for (int i = 0; i < 24; i++) {
+			if (i == location) {
+				panel.add(button);
+			} else {
+				JButton temp = (new JButton("            "));
+				temp.addActionListener(this);
+				panel.add(temp);
+			}
+
+		}
+		frame.add(panel);
+		button.addActionListener(this);
+		frame.setPreferredSize(new Dimension(300, 315));
+		frame.setTitle("WHACK-A-MOLE");
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton click = (JButton) e.getSource();
+		if (click == button) {
+			speak("HIT");
+			frame.dispose();
+			drawButtons(mole);
+		} else {
+			count +=1;
+			if(count ==5) {
+				speak("5 MISSES, GAME OVER");
+				System.exit(0);
+			}
+			speak("MISSED");
+			System.out.println("MISS");
+			frame.dispose();
+			drawButtons(mole);
+		}
+	}
+
+	static void speak(String words) {
+		if (System.getProperty("os.name").contains("Windows")) {
+			String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
+					+ words + "');\"";
+			try {
+				Runtime.getRuntime().exec(cmd).waitFor();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				Runtime.getRuntime().exec("say " + words).waitFor();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
